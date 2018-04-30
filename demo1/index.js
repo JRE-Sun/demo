a597bd2777b43c74ec5d03bc4817f51304eaa642
Sun Jre <jresun@163.com>
2018-04-30 10:20:38 +0800

    增加圆环加载

--- demo1/index.js
+++ demo1/index.js
@@ -1,10 +1,38 @@
 "use strict";
 
+function drawRound(index) {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.beginPath();
+    ctx.lineWidth = 8;
+    ctx.strokeStyle = '#ccc';
+    ctx.arc(90, 90, 74, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = 8;
+    ctx.strokeStyle = '#f30';
+    // 设置开始处为0点钟方向(-90 * Math.PI / 180)
+    // x为百分比值(0-100)
+    ctx.arc(90, 90, 74, -90 * Math.PI / 180, (index * 3.6 - 90) * Math.PI / 180);
+    ctx.stroke();
+    ctx.font = '40px Arial';
+    ctx.fillStyle = '#f30';
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+    ctx.fillText(index + '%', 90, 90);
+}
+
 new Progress({
     speedArray: [1, 5],
     endPositionArray: [75, 95],
+    init: function init(index) {
+        drawRound(index);
+    },
     loading: function loading(index) {
+        drawRound(index);
         document.querySelector('#progress').style.cssText = "width:" + index + "%";
+        // drawRound(index);
     },
     onLoad: function onLoad() {
         setTimeout(function () {
