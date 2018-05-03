63ecda39e7d66f80d15082b09e87f4cfa66f5cae
Sun Jre <jresun@163.com>
2018-05-03 09:52:12 +0800

    add demo3

--- demo3/index.js
+++ demo3/index.js
@@ -2,10 +2,24 @@
 
 $(function () {
     new Carousel({
-        parentClass: '.swiper-wrapper',
-        childClass: '.swiper-slide',
-        autoPlay: '1000',
+        parentClass: '.swiper0',
+        childClass: '.slide0',
         seize: 1,
+        loop: true
+    });
+
+    new Carousel({
+        parentClass: '.swiper1',
+        childClass: '.slide1',
+        autoPlay: '4000',
+        seize: 1
+    });
+
+    new Carousel({
+        parentClass: '.swiper2',
+        childClass: '.slide2',
+        autoPlay: '4000',
+        seize: .7,
         loop: true
     });
 });
